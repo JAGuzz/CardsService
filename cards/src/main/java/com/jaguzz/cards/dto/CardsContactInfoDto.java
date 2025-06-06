cc0fdca3e857d37f628e66718321a79a27a83ed0
JAGuzz <122820569+JAGuzz@users.noreply.github.com>
2025-06-06 11:52:11 -0700

    profile files and configuration setted up

---
+++ cards/src/main/java/com/jaguzz/cards/dto/CardsContactInfoDto.java
@@ -0,0 +1,21 @@
+package com.jaguzz.cards.dto;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@ConfigurationProperties(prefix = "cards")
+@Getter
+@Setter
+public class CardsContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
+
+}
